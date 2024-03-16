b3d053402e9354e7843ce15d6ccb04f8425400c4
parisosuch-dev <parisosuch.dev@gmail.com>
2024-03-16 13:20:02 -0700

    Create TODO list and create models.

---
+++ TODO.md
@@ -0,0 +1,20 @@
+# TODO
+
+## Create Models
+
+[ ] Status: The status of an uptime request instance
+[ ] Service: The service an uptime is pinging
+
+## Create URL endpoints
+
+[ ] `/api/status/`
+
+[ ] `/api/service/`
+
+## Create Views
+
+[ ] GET `/api/status/`: Get status for services
+
+[ ] GET `/api/service/`: Get service or services
+
+[ ] POST `/api/service/`: Post a new service to ping
--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Service(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=255, null=True)
+    url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Status(models.Model):
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+    response_time = models.IntegerField()
+    success = models.BooleanField()
