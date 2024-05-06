308dada0ebf87c30bf84e575ed26dd30f08e5559
Paris Osuch <parisosuch.dev@gmail.com>
2024-05-05 21:45:33 -0700

    Create models and migrations.

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Service(models.Model):
+    """The service being watched"""
+    name = models.TextField()
+    description = models.TextField()
+    url = models.URLField()
+    time_created = models.DateTimeField(auto_now_add=True)
+
+class Status(models.Model):
+    """An instance of an uptime check"""
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+    response_time = models.IntegerField() # in ms
+    is_up = models.BooleanField()
+    time = models.DateTimeField()
