040bed05b16076160a4b560ef28911502a9f68fc
Paris Osuch <parisosuch.dev@gmail.com>
2024-05-07 19:50:54 -0700

    Add django cors headers to req file

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,8 @@
-from django.urls import path, include
+from django.urls import path
+
+from .views import ServicesView, SingleServiceView
 
 urlpatterns = [
-    
+    path("service/", ServicesView.as_view()),
+    path("service/<str:service>/", SingleServiceView.as_view()),
 ]
