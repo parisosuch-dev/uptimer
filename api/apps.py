8d1d3c8ce75e73a6fbab03ecc17ae251373b388b
parisosuch-dev <parisosuch.dev@gmail.com>
2024-05-17 19:28:18 -0700

    Create scheduler for uptimer

--- api/apps.py
+++ api/apps.py
@@ -2,5 +2,14 @@ from django.apps import AppConfig
 
 
 class ApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'api'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "api"
+
+    def ready(self):
+        from .scheduler import start_scheduler
+
+        try:
+            start_scheduler()
+        except Exception as err:
+            print("There was an error trying to start scheduler!")
+            print(err)
