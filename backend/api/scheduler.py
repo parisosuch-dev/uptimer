2c4550417f7d427ef7462c40ccd0b0e7a18c0e51
parisosuch-dev <parisosuch.dev@gmail.com>
2024-06-23 09:56:27 -0700

    add env variable for period

--- backend/api/scheduler.py
+++ backend/api/scheduler.py
@@ -1,10 +1,24 @@
+import os
+
+from pathlib import Path
 from apscheduler.schedulers.background import BackgroundScheduler
+from dotenv import load_dotenv
+
 from .uptime import log_uptimes
 
 
 def start_scheduler():
     """Start background scheduler for uptime logging."""
+    # get retention environment variable (if none is present default will be used)
+    if Path(".env").exists():
+        load_dotenv()
+        period = os.getenv("UPTIMER_PING_PERIOD")
+    if not period:
+        seconds = 300
+    else:
+        seconds = int(period)
+
     scheduler = BackgroundScheduler()
-    scheduler.add_job(log_uptimes, "interval", minutes=5)
+    scheduler.add_job(log_uptimes, "interval", seconds=seconds)
 
     scheduler.start()
