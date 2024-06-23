cce949e5ed9bd113dbbe7b88985895fe814331be
parisosuch-dev <parisosuch.dev@gmail.com>
2024-06-23 10:02:52 -0700

    Make it half of the page

--- frontend/app/services/page.tsx
+++ frontend/app/services/page.tsx
@@ -2,15 +2,15 @@ import { getServices, Service } from "@/lib/uptimer";
 import ServiceCard from "@/components/service-card";
 
 export default async function ServicesPage() {
-    const services: Service[] = await getServices();
+  const services: Service[] = await getServices();
 
-    return (
-        <div className="flex min-h-screen flex-col items-center space-y-12 p-12">
-            <div className="w-1/4 p-4 space-y-4">
-                {services.map((service) => (
-                    <ServiceCard key={service.name} service={service} />
-                ))}
-            </div>
-        </div>
-    )
+  return (
+    <div className="flex min-h-screen flex-col items-center space-y-12 p-12">
+      <div className="w-1/2 p-4 space-y-4">
+        {services.map((service) => (
+          <ServiceCard key={service.name} service={service} />
+        ))}
+      </div>
+    </div>
+  );
 }
