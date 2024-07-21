450071c6b464720d19381e9500bb639fa8ec95de
parisosuch-dev <parisosuch.dev@gmail.com>
2024-07-21 13:07:46 -0700

    add time period picker

--- frontend/app/services/[serviceName]/page.tsx
+++ frontend/app/services/[serviceName]/page.tsx
@@ -1,36 +1,48 @@
-import { getService, Service } from "@/lib/uptimer";
-import { Card } from "@tremor/react";
-import { Tab, TabGroup, TabList } from "@tremor/react";
+"use client";
 
-export default async function ServicesPage({
+import { useState, useEffect } from "react";
+import { getService, Service } from "@/lib/uptimer";
+import { Select, SelectItem } from "@tremor/react";
+
+export default function ServicesPage({
   params,
 }: {
   params: { serviceName: string };
 }) {
-  const service: Service = await getService(params.serviceName);
+  const [service, setService] = useState<Service | null>(null);
+  const [period, setPeriod] = useState("w");
+
+  getService(params.serviceName).then((res) => {
+    setService(res);
+  });
+
+  useEffect(() => {}, [period]);
 
   return (
     <div className="flex flex-1 flex-col items-center p-8 w-full">
-      <div className="w-1/2 flex flex-row items-center justify-end">
-        <div className="w-1/2">
-          <div className="flex flex-row items-center space-x-2">
-            <h1 className="text-lg font-medium">{service.name}</h1>
-            <p className="text-xs bg-gray-100 px-1 rounded-lg font-mono">
-              {service.hostname}
-            </p>
+      {service ? (
+        <div className="w-1/2 flex flex-row items-center justify-end">
+          <div className="w-3/4">
+            <div className="flex flex-row items-center space-x-2">
+              <h1 className="text-lg font-medium">{service.name}</h1>
+              <p className="text-xs bg-gray-100 px-1 rounded-lg font-mono">
+                {service.hostname}
+              </p>
+              {period}
+            </div>
+            <p className="text-sm text-gray-500">{service.description}</p>
           </div>
-          <p className="text-sm text-gray-500">{service.description}</p>
+          <div className="w-1/4 flex justify-end">
+            <Select defaultValue="w" name="period" onValueChange={setPeriod}>
+              <SelectItem value="m">Month</SelectItem>
+              <SelectItem value="w">Week</SelectItem>
+              <SelectItem value="d">Day</SelectItem>
+            </Select>
+          </div>
         </div>
-        <div className="w-1/2 flex justify-end">
-          <TabGroup className="w-1/2">
-            <TabList variant="solid">
-              <Tab>month</Tab>
-              <Tab>week</Tab>
-              <Tab>day</Tab>
-            </TabList>
-          </TabGroup>
-        </div>
-      </div>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
