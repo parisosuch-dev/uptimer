46ade73afd2a2fe92e43edcc7da802f0584040cc
parisosuch-dev <parisosuch.dev@gmail.com>
2024-07-27 15:22:25 -0700

    create period picker for statuses and then receive statuses for that given period

--- frontend/app/services/[serviceName]/page.tsx
+++ frontend/app/services/[serviceName]/page.tsx
@@ -11,38 +11,13 @@ export default function ServicesPage({
   params: { serviceName: string };
 }) {
   const [service, setService] = useState<Service | null>(null);
-  const [statuses, setStatuses] = useState<Status[] | []>([]);
   const [period, setPeriod] = useState("w");
 
-  getService(params.serviceName).then((res) => {
-    setService(res);
-  });
-
-  const calculateStartDate = (period: string) => {
-    const today = new Date();
-    const start = new Date();
-
-    let days = 7;
-    if (period === "m") {
-      days = 30;
-    }
-    if (period === "d") {
-      days = 1;
-    }
-
-    start.setDate(today.getDate() - days);
-
-    return start.toISOString().split("T")[0];
-  };
-
   useEffect(() => {
-    if (service) {
-      const start = calculateStartDate(period);
-      getStatuses({ service: service?.name, start: start }).then((res) =>
-        setStatuses(res)
-      );
-    }
-  }, [service, period]);
+    getService(params.serviceName).then((res) => {
+      setService(res);
+    });
+  }, []);
 
   if (service) {
     return (
@@ -65,7 +40,7 @@ export default function ServicesPage({
             </Select>
           </div>
         </div>
-        <ServiceStats statuses={statuses} />
+        <ServiceStats service={service} period={period} />
       </div>
     );
   }
--- frontend/components/service-stats.tsx
+++ frontend/components/service-stats.tsx
@@ -1,3 +1,34 @@
-export default function ServiceStats() {
-  return <div>service stats</div>;
+"use client"
+
+
+import { useState, useEffect } from "react";
+import { Service, Status, getStatuses } from "@/lib/uptimer";
+
+export default function ServiceStats({ service, period }: { service: Service, period: string }) {
+  const [statuses, setStatuses] = useState<Status[] | []>([]);
+
+  const calculateStartDate = (period: string) => {
+    const today = new Date();
+    const start = new Date();
+
+    let days = 7;
+    if (period === "m") {
+      days = 30;
+    }
+    if (period === "d") {
+      days = 1;
+    }
+
+    start.setDate(today.getDate() - days);
+
+    return start.toISOString().split("T")[0];
+  };
+
+  useEffect(() => {
+    // calculate start date and get statuses
+    const start = calculateStartDate(period);
+    getStatuses({ service: service.name, start: start }).then((res) => setStatuses(res));
+  }, [period])
+
+  return <div>statuses: {statuses.length}</div>;
 }
