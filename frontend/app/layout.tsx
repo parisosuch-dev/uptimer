f73c46c954501e9b7c3392ad691c0e40dc22f107
parisosuch-dev <parisosuch.dev@gmail.com>
2024-07-29 19:01:00 -0700

    add some more margin space

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
       <body className="bg-gray-50 h-screen flex flex-col">
-        <div className="w-full flex justify-between py-8 px-16">
+        <div className="w-full flex justify-between py-8 px-24">
           <Link href="/" className="text-2xl font-bold">
             uptimer.
           </Link>
